d7cbf1f4a03d6ef3ee6d2f3562ca19164eff0afd
joeyyoung <1761889730@qq.com>
2021-11-02 20:17:32 +0800

    slp before allreduce

--- test/nccl-perf/all_reduce.cu
+++ test/nccl-perf/all_reduce.cu
@@ -104,6 +104,9 @@ int main(int argc, char *argv[]) {
     // initializing NCCL
     NCCLCHECK(ncclCommInitRank(&comm, nRanks, id, myRank));
 
+    printf("Ready to do all_reduce, sleep 5 seconds...");
+    sleep(5);
+
     // communicating using NCCL
     NCCLCHECK(ncclAllReduce((const void *) sendbuff, (void *) recvbuff,
                             size, ncclFloat, ncclSum,
