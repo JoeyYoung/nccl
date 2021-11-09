a7c66f2499eae5fd8c27dcecad4ccc32ea71988c
joeyyoung <1761889730@qq.com>
2021-11-10 00:02:45 +0800

    ip done, todo fd

--- src/collectives/all_reduce.cc
+++ src/collectives/all_reduce.cc
@@ -8,13 +8,19 @@
 #include "mlcc.h"
 
 
-// todo, send to ccp ? or control in connectAddress, for local flow without setting ccp (not init problem)
+/*
+todo,
+now socket number correct, keep tracking, if fail just ignore socket id?
+  (1) we check only send socket with ccp option, will be recorded in ccp agent.
+  (2) send to ccp ? or control in connectAddress, for local flow without setting ccp (not init problem)
+*/
+
 void checkRankIndex(ncclComm* comm){
   // we only use one channel for each connection
   int myRank = comm->channels[0].ring.index;
   int nextRank = comm->channels[0].ring.next;
   int preRank = comm->channels[0].ring.prev;
-  printf("[all_reduce.cc] One tensor will be sent from rank %d (ip:%s) to rank %d (ip: %s) with socket fd\n", myRank, myRankIP, nextRank, nextRankIP);
+  printf("[all_reduce.cc] One tensor will be sent from rank %d (ip:%s) to rank %d (ip: %s)\n", myRank, myRankIP, nextRank, nextRankIP);
 }
 
 NCCL_API(ncclResult_t, ncclAllReduce, const void* sendbuff, void* recvbuff, size_t count,
